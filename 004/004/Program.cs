be64b62a5e6b85ef2460f415ede4c8e38e40a408
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2021-12-29 21:24:44 -0500

    homework
    Homework added

---
+++ 004/004/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello World");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.BackgroundColor++;
+                Console.WriteLine("Hello World");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 008/008/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 12;
+            int z = 3;
+
+
+
+            int result = (x += y - x++ * z);
+            int result2 = (z = --x - y * 5);
+            int result3 = (y /= x + 5 % z);
+            int result4 = (z = x++ + y * 5);
+            int result5 = (x = y - x++ * z);
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+            Console.WriteLine(result5);
+            Console.ReadLine();
+        }
+    }
+}
