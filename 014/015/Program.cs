113d531814f3f55e6802aaab70ed85103a36987b
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-01-02 16:37:30 -0500

    homework 15
    excercise 15

---
+++ 014/015/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please Enter Quantity Of The Product: ");
+            double quantityOfProduct = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter The Price for 1m");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            double totalPrice = (quantityOfProduct * price);
+            double sumOfDiscount = (totalPrice * 0.1); //discount is 10%
+            double totalWithDiscountApplied = (totalPrice - sumOfDiscount);
+            Console.WriteLine(" Total Ammount: " + totalPrice + 
+                "\n Summ of Discount: " + sumOfDiscount + "\n Total: " + totalWithDiscountApplied);
+            Console.ReadLine();
+        }
+    }
+}
