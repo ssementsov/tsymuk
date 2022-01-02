2e822fc6721f08da788b02173b2f5d8e03ba42bc
SergeTsymuk <96663573+SergeTsymuk@users.noreply.github.com>
2022-01-02 13:18:50 -0500

    homework
    new homework added

---
+++ 013/013/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool retired = true; bool working = true; bool unemployed = true;
+
+            bool subsidyAllowed = (unemployed && retired || !working && !retired && unemployed);
+            Console.WriteLine(subsidyAllowed);
+            Console.ReadLine();
+        }
+    }
+}
